acd47604277fc5b950e4a7ac8751a86b7b8c89b6
niuta24 <syvkova.pn@ucu.edu.ua>
2023-10-26 18:27:11 +0300

    commit 10

--- labseven/src/main/java/ua/edu/ucu/labseven/flowers/FlowerController.java
+++ labseven/src/main/java/ua/edu/ucu/labseven/flowers/FlowerController.java
@@ -3,11 +3,13 @@ package ua.edu.ucu.labseven.flowers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class FlowerController {
-    @GetMapping
+    @GetMapping("/flowers")
     public List<Flower> getFlowers() {
         return List.of(new Flower(1.0, FlowerColor.BLUE,
                 1.0, FlowerType.CHAMOMILE));
