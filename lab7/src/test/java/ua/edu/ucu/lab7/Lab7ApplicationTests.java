62cca3b478515e6058cea6e866d816408f6c1776
niuta24 <syvkova.pn@ucu.edu.ua>
2023-10-26 12:00:02 +0300

    commit

--- lab7/src/test/java/ua/edu/ucu/lab7/Lab7ApplicationTests.java
+++ lab7/src/test/java/ua/edu/ucu/lab7/Lab7ApplicationTests.java
@@ -3,6 +3,10 @@ package ua.edu.ucu.lab7;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ua.edu.ucu.lab7.flowers.Flower;
+import ua.edu.ucu.lab7.flowers.FlowerColor;
+import ua.edu.ucu.lab7.flowers.FlowerType;
+
 @SpringBootTest
 class Lab7ApplicationTests {
 
@@ -10,4 +14,14 @@ class Lab7ApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testMain() {
+		Lab7Application.main(new String[] {});
+	}
+
+	@Test
+	void testFlower() {
+		Flower flower = new Flower(1.0, FlowerColor.BLUE, 1.0, FlowerType.CHAMOMILE);
+		assert flower.getPrice() == 1.0;
+	}
 }
