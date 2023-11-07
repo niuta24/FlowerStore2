d02d6fc33f734befb2aa1b8c447b451ef29a831a
niuta24 <syvkova.pn@ucu.edu.ua>
2023-11-07 12:53:56 +0300

    Linter

--- src/main/java/ua/edu/ucu/labseven/flowers/FlowerController.java
+++ src/main/java/ua/edu/ucu/labseven/flowers/FlowerController.java
@@ -1,8 +1,6 @@
 package ua.edu.ucu.labseven.flowers;
 
 import java.util.List;
-
-// import org.apache.catalina.connector.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
